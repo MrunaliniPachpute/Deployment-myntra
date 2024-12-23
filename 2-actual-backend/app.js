0fe7c0a6e2b2113eb93d689c8d45ab4d0968cf5e
MrunaliniPachpute <pachputemrunalini@gmail.com>
2024-12-24 02:19:56 +0530

    msg

--- 2-actual-backend/app.js
+++ 2-actual-backend/app.js
@@ -35,9 +35,9 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.get('/', (req, res) =>{
-//   res.send('Welcome')
-// })
+app.get('/', (req, res) =>{
+  res.send('Welcome')
+})
 
 app.get('/items', async (req, res) => {
   const storedItems = await getStoredItems();
