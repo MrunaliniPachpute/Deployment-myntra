1e1d6522132dff73a7823fec25aab76742fd9ae7
MrunaliniPachpute <pachputemrunalini@gmail.com>
2024-12-23 23:37:15 +0530

    lines added

--- 2-actual-backend/app.js
+++ 2-actual-backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const serverless = require('serverless-http');
 
 const { getStoredItems, storeItems } = require('./data/items');
 
@@ -42,3 +43,6 @@ app.post('/items', async (req, res) => {
 
 const port = process.env.PORT || 8080;
 app.listen(port, ()=>console.log(`port started at: ${port}`));
+
+module.exports = app;
+module.exports.handler = serverless(app);
